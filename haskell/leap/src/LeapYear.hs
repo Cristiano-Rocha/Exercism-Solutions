ad9af36f130b13a85416d90a4f7747d7ff82b603
Cristiano Ribeiro <cristiano.ribeiro@knewin.com>
2022-09-10 09:59:49 -0300

    elm hello world soln

--- haskell/leap/src/LeapYear.hs
+++
@@ -1,9 +0,0 @@
-module LeapYear (isLeapYear) where
-
-isLeapYear :: Integer -> Bool
-isLeapYear year
-  | mod year 400 == 0 = True
-  | mod year 100 == 0 = False
-  | mod year 4 == 0 = True
-  | otherwise = False
-
